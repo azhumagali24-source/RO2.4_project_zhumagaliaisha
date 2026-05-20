71678b4f6c892fa5d4e1102f018182ef8ba4cf60
Your Name <ajsazumagali420>
2026-05-20 23:31:33 +0500

    Добавьте файлы проекта.

---
+++ StudentAPP/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentAPP;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        CounterBtn.Text = $"Clicked {count} time(s)";
+    }
+
+    private async void OnOpenStudentsClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(StudentsPage));
+    }
+}
---
+++ StudentAPP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StudentAPP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
